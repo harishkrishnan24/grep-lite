500f6afb36347f9186ec2696f25657c1b480ee2a
Harish Krishnan <harishkrishnan1993@gmail.com>
2022-12-18 17:27:52 +0530

    Find searches from stdin and files

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,51 @@
-fn main() {
-    let search_term = "picture";
-    let quote = "\
-        Every face, every shop, bedroom window, public-house, and
-        dark square is a picture feverishly turned--in search of what?
-        It is the same with books.
-        What do we seek through millions of pages?";
+use std::{
+    fs::File,
+    io::{self, BufRead, BufReader},
+};
 
-    for (i, line) in quote.lines().enumerate() {
-        if line.contains(search_term) {
-            let line_num = i + 1;
-            println!("{}: {}", line_num, line);
+use clap::{App, Arg};
+use regex::Regex;
+
+fn process_lines<T: BufRead + Sized>(reader: T, re: Regex) {
+    for line_ in reader.lines() {
+        let line = line_.unwrap();
+        match re.find(&line) {
+            Some(_) => println!("{}", line),
+            None => (),
         }
+    }
+}
+
+fn main() {
+    let args = App::new("grep-lite")
+        .version("0.1")
+        .about("searches for patterns")
+        .arg(
+            Arg::with_name("pattern")
+                .help("The pattern to search for")
+                .takes_value(true)
+                .required(true),
+        )
+        .arg(
+            Arg::with_name("input")
+                .help("File to search")
+                .takes_value(true)
+                .required(true),
+        )
+        .get_matches();
+
+    let pattern = args.value_of("pattern").unwrap();
+    let re = Regex::new(pattern).unwrap();
+
+    let input = args.value_of("input").unwrap_or("-");
+
+    if input == "-" {
+        let stdin = io::stdin();
+        let reader = stdin.lock();
+        process_lines(reader, re);
+    } else {
+        let f = File::open(input).unwrap();
+        let reader = BufReader::new(f);
+        process_lines(reader, re)
     }
 }
